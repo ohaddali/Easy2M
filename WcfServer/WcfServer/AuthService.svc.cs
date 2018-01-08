1650171c544f03c4a5333e48fe79013355e78541
naordal <naordal@LB-003-25>
2018-01-08 21:14:30 +0200

    1123

---
+++ WcfServer/WcfServer/AuthService.svc.cs
@@ -0,0 +1,46 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Runtime.Serialization;
+using System.ServiceModel;
+using System.ServiceModel.Web;
+using System.Text;
+
+
+namespace WcfServer
+{
+    // NOTE: You can use the "Rename" command on the "Refactor" menu to change the class name "Service1" in code, svc and config file together.
+    // NOTE: In order to launch WCF Test Client for testing this service, please select Service1.svc or Service1.svc.cs at the Solution Explorer and start debugging.
+    public class AuthService : IAuthService
+    {
+        public bool login(string userName, string password)
+        {
+            Easy2MEntities ent = new Easy2MEntities();
+            
+            var L2EQuery = from u in ent.Users
+            where u.userName == userName && u.password == password
+            select u;
+
+            var user = L2EQuery.FirstOrDefault<User>();
+
+            return user != null;
+        }
+
+        public bool register(string userName, string password, string admin)
+        {
+            bool ad;
+            bool.TryParse(admin, out ad);
+            User user = new User()
+            {
+                userName = userName,
+                password = password,
+                admin = ad
+            };
+
+            Easy2MEntities ent = new Easy2MEntities();
+            ent.Users.Add(user);
+            ent.SaveChanges();
+            return true;
+        }
+    }
+}
---
+++ WcfServer/WcfServer/IAuthService.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Runtime.Serialization;
+using System.ServiceModel;
+using System.ServiceModel.Web;
+using System.Text;
+
+namespace WcfServer
+{
+    // NOTE: You can use the "Rename" command on the "Refactor" menu to change the interface name "IService1" in both code and config file together.
+    [ServiceContract]
+    public interface IAuthService
+    {
+
+        [OperationContract]
+        [WebInvoke(Method = "GET" , UriTemplate = "register/{userName}/{password}/{admin}") ]
+        bool register(string userName, string password , string admin);
+
+        [OperationContract]
+        [WebInvoke(Method = "GET", UriTemplate = "login/{userName}/{password}")]
+        bool login(string userName , string password);
+
+        // TODO: Add your service operations here
+    }
+
+}
