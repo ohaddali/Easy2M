aedee92adc4bb586a59f513cd080051d7d008c40
naordalal <naordal@post.bgu.ac.il>
2018-02-18 19:05:47 +0200

    request shifts method

---
+++ WcfServer/WcfServer/IShiftRequestService.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Runtime.Serialization;
+using System.ServiceModel;
+using System.ServiceModel.Web;
+using System.Text;
+
+namespace WcfServer
+{
+    // NOTE: You can use the "Rename" command on the "Refactor" menu to change the interface name "IShiftRequestService" in both code and config file together.
+    [ServiceContract]
+    public interface IShiftRequestService
+    {
+        [OperationContract]
+        [WebInvoke(Method = "POST", RequestFormat = WebMessageFormat.Json, BodyStyle = WebMessageBodyStyle.WrappedRequest)]
+        bool requestShift(int week , int year , long workerId);
+
+        [OperationContract]
+        [WebInvoke(Method = "POST", RequestFormat = WebMessageFormat.Json, BodyStyle = WebMessageBodyStyle.WrappedRequest)]
+        bool cancelRequest(ShiftRequest request);
+    }
+}
